e56990b712d9ff0f0444c908ef2eb6546083c58f
popetwo-2 <109599940+popetwo-2@users.noreply.github.com>
2023-04-12 09:39:25 +0100

    urls.py update

--- account/urls.py
+++ account/urls.py
@@ -20,7 +20,7 @@ from . import views
 urlpatterns = [
     path('accountinfo/', views.GetAccountInfo, name='home'),
     path('single_info/<int:login>/', views.get_account_info_single, name='single'),
-    path('/', views.home, name='index'),
+    path('', views.home, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('challenges/', views.challenge, name='challenge')
 ]
